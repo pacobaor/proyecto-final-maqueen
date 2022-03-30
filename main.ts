cb5bf29c18043ed425d715c56e00ff36ef0d88e5
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-03-30 09:28:18 +0200

    Escriba un nombre para su proyecto Id333uwy7y3

--- main.ts
+++ main.ts
@@ -13,7 +13,17 @@ radio.onReceivedNumber(function (receivedNumber) {
         maqueen.motorStop(maqueen.Motors.All)
         basic.showIcon(IconNames.No)
     }
+    if (receivedNumber == 4) {
+        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    }
 })
 radio.setGroup(333)
 music.playMelody("C5 A B G A F G E ", 120)
 basic.showString("Hola")
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+})
