cdc5c04edaf8abdcdb7996d16f1555806d3dbe56
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-03-30 09:12:25 +0200

    udifv8fh

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (receivedNumber == 2) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showIcon(IconNames.No)
+    }
+})
 radio.setGroup(333)
 music.playMelody("C5 A B G A F G E ", 120)
 basic.showString("Hola")
