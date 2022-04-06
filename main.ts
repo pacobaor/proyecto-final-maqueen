9584f37888c7ef108f1df91cf3c91961e64f7c3d
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-04-06 09:06:21 +0200

    uohwf9wfh

--- main.ts
+++ main.ts
@@ -17,7 +17,25 @@ radio.onReceivedNumber(function (receivedNumber) {
         maqueen.motorStop(maqueen.Motors.M2)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
     }
+    if (receivedNumber == 5) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    }
+    if (receivedNumber == 6) {
+        seguir_la_línea()
+    }
 })
+function seguir_la_línea () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorStop(maqueen.Motors.M2)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorStop(maqueen.Motors.M1)
+    }
+}
 radio.setGroup(333)
 music.playMelody("C5 A B G A F G E ", 120)
 basic.showString("Hola")
@@ -28,8 +46,9 @@ basic.forever(function () {
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 })
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 6) {
-        radio.sendNumber(3)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 6) {
+        maqueen.motorStop(maqueen.Motors.All)
         basic.showIcon(IconNames.No)
+        radio.sendNumber(7)
     }
 })
